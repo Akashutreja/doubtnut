adceeb9fd296424abb97bba1bbee6e1b6b1eac1c
akashutreja <akash.utreja@loconav.com>
2020-05-18 13:16:28 +0530

    cleaned extra code

--- account/cron.py
+++
@@ -1,9 +0,0 @@
-from .models import UserProfile
-import datetime
-
-def my_cron_job():
-  active_on = datetime.datetime.now()
-  next_five_minute = active_on - datetime.timedelta(minute = 5)
-  print(active_on, next_five_minute)
-  queryset = UserProfile.filter(last_activity__range=(active_on, next_five_minute) )
-  print(queryset.count)
